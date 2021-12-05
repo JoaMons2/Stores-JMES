ea04225636f8445e172c1fad8865d47998d86e75
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2021-12-05 17:42:40 -0600

    MenúEnFragment

--- app/src/main/java/com/example/stores/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores/EditStoreFragment.kt
@@ -1,18 +1,15 @@
 package com.example.stores
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.stores.databinding.FragmentEditStoreBinding
 
 class EditStoreFragment : Fragment() {
 
     private lateinit var mBinding: FragmentEditStoreBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle? ): View? {
         mBinding = FragmentEditStoreBinding.inflate(inflater, container, false)
 
@@ -25,5 +22,12 @@ class EditStoreFragment : Fragment() {
         val activity = activity as? MainActivity
         activity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
         activity?.supportActionBar?.title = getString(R.string.edit_store_title_add)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
