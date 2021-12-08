3266a46e897ceffd4c7fdd0aafd02d1a0f666be7
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2021-12-07 18:02:40 -0600

    OcultarElTeclado

--- app/src/main/java/com/example/stores/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores/EditStoreFragment.kt
@@ -1,7 +1,9 @@
 package com.example.stores
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import com.example.stores.databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
@@ -49,17 +51,32 @@ class EditStoreFragment : Fragment() {
                 doAsync {
                     StoreApplication.database.storeDao().addStore(store)
                     uiThread {
+                        hideKeyboard()
+
                         Snackbar.make(mBinding.root,
                             getString(R.string.edit_store_message_save_sucess),
                             Snackbar.LENGTH_SHORT)
                             .show()
+
+                        mActivity?.onBackPressed()
                     }
                 }
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
-        //return super.onOptionsItemSelected(item)
+    }
+
+    private fun hideKeyboard(){
+        val imm = mActivity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (view != null){
+            imm.hideSoftInputFromWindow(view!!.windowToken, 0)
+        }
+    }
+
+    override fun onDestroyView() {
+        hideKeyboard()
+        super.onDestroyView()
     }
 
     override fun onDestroy() {
